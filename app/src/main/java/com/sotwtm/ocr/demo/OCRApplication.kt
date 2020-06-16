eee466f6551fbe3589a0db9a9ab4631af12c71e1
John Tsai <sheungon@gmail.com>
2020-06-16 17:30:32 +0800

    Updated all libs. Updated minSdkVersion. Removed Fabric.

--- app/src/main/java/com/sotwtm/ocr/demo/OCRApplication.kt
+++ app/src/main/java/com/sotwtm/ocr/demo/OCRApplication.kt
@@ -1,12 +1,8 @@
 package com.sotwtm.ocr.demo
 
 import android.app.Application
-import android.os.Build
 import android.webkit.WebView
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.sotwtm.util.Log
-import io.fabric.sdk.android.Fabric
 
 /**
  * Application class of OCR Demo app
@@ -17,26 +13,12 @@ class OCRApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        initCrashReporter()
-
         Log.logLevel = if (BuildConfig.DEBUG) Log.VERBOSE else Log.NONE
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            try {
-                WebView.setWebContentsDebuggingEnabled(Log.isDebuggable)
-            } catch (e: Exception) {
-                Log.e("Error on enabling web debug", e)
-            }
+        try {
+            WebView.setWebContentsDebuggingEnabled(Log.isDebuggable)
+        } catch (e: Exception) {
+            Log.e("Error on enabling web debug", e)
         }
-    }
-
-    private fun initCrashReporter() {
-        val core = CrashlyticsCore.Builder()
-            .disabled(!BuildConfig.USE_CRASHLYTICS)
-            .build()
-        val crashlytics = Crashlytics.Builder()
-            .core(core)
-            .build()
-        Fabric.with(this, crashlytics)
     }
 }
